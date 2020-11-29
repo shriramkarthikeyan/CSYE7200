393992109c31145618dca22800faaf2ffb20be19
Ram Karthikeyan <shriram.karthikeyan@gmail.com>
2020-11-28 21:10:35 -0500

    dropped columns and added decision tree code

--- src/main/scala/test.scala
+++ src/main/scala/test.scala
@@ -1,9 +1,12 @@
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext, sql}
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.monotonically_increasing_id
+import org.apache.spark.sql.functions.{monotonically_increasing_id, when}
 //import sparkObject.spark.implicits._
 import org.apache.spark.sql.functions.split
+import org.apache.spark.mllib.tree.DecisionTree
+import org.apache.spark.mllib.tree.model.DecisionTreeModel
+import org.apache.spark.mllib.util.MLUtils
 
 object test {
 
@@ -17,6 +20,7 @@ object test {
       .option("inferSchema", "true")
       .load("C:\\Users\\shrir\\OneDrive - Northeastern University\\CSYE7200 Big Data Systems Enginnering with " +
         "Scala - Project\\Data\\person\\*.csv")
+
 
     personDF = personDF.drop("CRASH_DATETIME"
       ,"CRASH_STATUS"
@@ -42,7 +46,52 @@ object test {
       ,"SCHL_BUS_RELD_DESCR"
       ,"WORK_ZONE_RELD_DESCR"
       ,"HAZ_MAT_PLACARD_DESCR"
-      ,"VEHC_REG_TYPE_CODE")
+      ,"VEHC_REG_TYPE_CODE"
+      ,"NON_MTRST_TYPE_CL"
+      ,"NON_MTRST_ACTN_CL"
+      ,"NON_MTRST_LOC_CL"
+      ,"AADT"
+      ,"AADT_YEAR"
+      ,"PK_PCT_SUT"
+      ,"AV_PCT_SUT"
+      ,"PK_PCT_CT"
+      ,"AV_PCT_CT"
+      ,"CURB"
+      ,"TRUCK_RTE"
+      ,"LT_SIDEWLK"
+      ,"RT_SIDEWLK"
+      ,"SHLDR_LT_W"
+      ,"SHLDR_LT_T"
+      ,"SURFACE_WD"
+      ,"SHLDR_RT_W"
+      ,"SHLDR_RT_T"
+      ,"OPP_LANES"
+      ,"MED_WIDTH"
+      ,"MED_TYPE"
+      ,"URBAN_TYPE"
+      ,"F_CLASS"
+      ,"URBAN_AREA"
+      ,"FD_AID_RTE"
+      ,"FACILITY"
+      ,"OPERATION"
+      ,"CONTROL"
+      ,"PEAK_LANE"
+      ,"STREETNAME"
+      ,"FROMSTREETNAME"
+      ,"TOSTREETNAME"
+      ,"CITY"
+      ,"STRUCT_CND"
+      ,"TERRAIN"
+      ,"URBAN_LOC_TYPE"
+      ,"AADT_DERIV"
+      ,"STATN_NUM"
+      ,"OP_DIR_SL"
+      ,"SHLDR_UL_T"
+      ,"SHLDR_UL_W"
+      ,"T_EXC_TYPE"
+      ,"T_EXC_TIME"
+      ,"F_F_CLASS"
+    )
 
     personDF.printSchema()
 
@@ -63,13 +112,13 @@ object test {
     personDF.printSchema()
     personDF.select("DRIVER_AGE","AGE").show()
     filterAge(personDF,46).select("CRASH_NUMB","DRIVER_AGE").show()
-
+    personDF = personDF.withColumn("FATALITY_BIN", when($"NUMB_FATAL_INJR" === 0, 0).otherwise(1))
   }
 
-def filterAge(df:sql.DataFrame, age:Int) : sql.DataFrame = {
-  var df2 = df.filter(df("DRIVER_AGE") === age)
-  df2
-}
+  def filterAge(df:sql.DataFrame, age:Int) : sql.DataFrame = {
+    var df2 = df.filter(df("DRIVER_AGE") === age)
+    df2
+  }
 
 }
 
