0e6418526f25b47cfa642fa2a7441585e76e7690
Ram Karthikeyan <shriram.karthikeyan@gmail.com>
2020-12-09 23:37:22 -0500

    added datetime fields

--- src/main/scala/MLUseCase.scala
+++ src/main/scala/MLUseCase.scala
@@ -31,7 +31,7 @@ object MLUseCase {
         "Scala - Project\\Data\\person\\*.csv")
 
 
-    personDF = personDF.drop("CRASH_DATETIME","CRASH_STATUS","POLC_AGNCY_TYPE_DESCR","VEHC_TRVL_DIRC_CL","MM_RTE","DIST_DIRC_MILEMARKER","MILEMARKER","EXIT_RTE","EXIT_NUMB","DIST_DIRC_LANDMARK","LANDMARK","X","Y","RMV_DOC_IDS","CRASH_RPT_IDS","RPA_ABBR","VEHC_EMER_USE_CL","VEHC_TOWED_FROM_SCENE_CL","FMCSA_RPTBL_CL","FMCSA_RPTBL","ROAD_CNTRB_DESCR","SCHL_BUS_RELD_DESCR","WORK_ZONE_RELD_DESCR","HAZ_MAT_PLACARD_DESCR","VEHC_REG_TYPE_CODE","NON_MTRST_TYPE_CL","NON_MTRST_ACTN_CL","NON_MTRST_LOC_CL","AADT","AADT_YEAR","PK_PCT_SUT","AV_PCT_SUT","PK_PCT_CT","AV_PCT_CT","CURB","TRUCK_RTE","LT_SIDEWLK","RT_SIDEWLK","SHLDR_LT_W","SHLDR_LT_T","SURFACE_WD","SHLDR_RT_W","SHLDR_RT_T","OPP_LANES","MED_WIDTH","MED_TYPE","URBAN_TYPE","F_CLASS","URBAN_AREA","FD_AID_RTE","FACILITY","OPERATION","CONTROL","PEAK_LANE","STREETNAME","FROMSTREETNAME","TOSTREETNAME","CITY","STRUCT_CND","TERRAIN","URBAN_LOC_TYPE","AADT_DERIV","STATN_NUM","OP_DIR_SL","SHLDR_UL_T","SHLDR_UL_W","T_EXC_TYPE","T_EXC_TIME","F_F_CLASS"
+    personDF = personDF.drop("CRASH_STATUS","POLC_AGNCY_TYPE_DESCR","VEHC_TRVL_DIRC_CL","MM_RTE","DIST_DIRC_MILEMARKER","MILEMARKER","EXIT_RTE","EXIT_NUMB","DIST_DIRC_LANDMARK","LANDMARK","X","Y","RMV_DOC_IDS","CRASH_RPT_IDS","RPA_ABBR","VEHC_EMER_USE_CL","VEHC_TOWED_FROM_SCENE_CL","FMCSA_RPTBL_CL","FMCSA_RPTBL","ROAD_CNTRB_DESCR","SCHL_BUS_RELD_DESCR","WORK_ZONE_RELD_DESCR","HAZ_MAT_PLACARD_DESCR","VEHC_REG_TYPE_CODE","NON_MTRST_TYPE_CL","NON_MTRST_ACTN_CL","NON_MTRST_LOC_CL","AADT","AADT_YEAR","PK_PCT_SUT","AV_PCT_SUT","PK_PCT_CT","AV_PCT_CT","CURB","TRUCK_RTE","LT_SIDEWLK","RT_SIDEWLK","SHLDR_LT_W","SHLDR_LT_T","SURFACE_WD","SHLDR_RT_W","SHLDR_RT_T","OPP_LANES","MED_WIDTH","MED_TYPE","URBAN_TYPE","F_CLASS","URBAN_AREA","FD_AID_RTE","FACILITY","OPERATION","CONTROL","PEAK_LANE","STREETNAME","FROMSTREETNAME","TOSTREETNAME","CITY","STRUCT_CND","TERRAIN","URBAN_LOC_TYPE","AADT_DERIV","STATN_NUM","OP_DIR_SL","SHLDR_UL_T","SHLDR_UL_W","T_EXC_TYPE","T_EXC_TIME","F_F_CLASS"
     )
 
     personDF.printSchema()
@@ -53,11 +53,11 @@ object MLUseCase {
 //    personDF.select("DRIVER_AGE","AGE").show()
 //    filterAge(personDF,46).select("CRASH_NUMB","DRIVER_AGE").show()
     personDF = personDF.withColumn("FATALITY_BIN", when(col("NUMB_FATAL_INJR") === 0, 0).otherwise(1))
-    var col_array = Array("LAT","LON","DISTRICT_NUM","LCLTY_NAME","OWNER_ADDR_CITY_TOWN","OWNER_ADDR_STATE","VEHC_REG_STATE","WEATH_COND_DESCR","ROAD_SURF_COND_DESCR","MAX_INJR_SVRTY_CL","MANR_COLL_DESCR","FIRST_HRMF_EVENT_DESCR","MOST_HRMFL_EVT_CL","DRVR_CNTRB_CIRC_CL","VEHC_CONFIG_CL","HIT_RUN_DESCR","AGE_DRVR_YNGST","AGE_DRVR_OLDEST","DRVR_DISTRACTED_CL","DRIVER_AGE","DRIVER_DISTRACTED_TYPE_DESCR","DRVR_LCN_STATE","DRUG_SUSPD_TYPE_DESCR","SFTY_EQUP_DESC_1","SFTY_EQUP_DESC_2","ALC_SUSPD_TYPE_DESCR","FATALITY_BIN")
     val personDFSubset = personDF.select(
 
       "LAT"
       ,"LON"
+      ,"CRASH_DATETIME"
       ,"DISTRICT_NUM"
       ,"LCLTY_NAME"
       ,"OWNER_ADDR_CITY_TOWN"
@@ -69,27 +69,27 @@ object MLUseCase {
       ,"MANR_COLL_DESCR"
       ,"FIRST_HRMF_EVENT_DESCR"
 //      ,"MOST_HRMFL_EVT_CL"
-      ,"DRVR_CNTRB_CIRC_CL"
       ,"VEHC_CONFG_DESCR"
-      ,"HIT_RUN_DESCR"
+//      ,"HIT_RUN_DESCR"
       ,"AGE_DRVR_YNGST"
       ,"AGE_DRVR_OLDEST"
-      ,"DRVR_DISTRACTED_CL"
+//      ,"DRVR_DISTRACTED_CL"
+      ,"DRVR_CNTRB_CIRC_CL"
       ,"DRIVER_AGE"
       ,"DRIVER_DISTRACTED_TYPE_DESCR"
       ,"DRVR_LCN_STATE"
       ,"DRUG_SUSPD_TYPE_DESCR"
-      ,"SFTY_EQUP_DESC_1"
-      ,"SFTY_EQUP_DESC_2"
+//      ,"SFTY_EQUP_DESC_1"
+//      ,"SFTY_EQUP_DESC_2"
       ,"ALC_SUSPD_TYPE_DESCR"
       ,"FATALITY_BIN"
     )
     println("personDF_split_cols")
-    val personDF_split_cols = split_columns(personDF,"VEHC_SEQ_EVENTS")
-    personDF_split_cols
-      .select("CRASH_NUMB","Vehicle_One","Vehicle_Config")
-      .show()
-    personDF_split_cols.printSchema()
+//    val personDF_split_cols = split_columns(personDF,"VEHC_SEQ_EVENTS")
+//    personDF_split_cols
+//      .select("CRASH_NUMB","Vehicle_One","Vehicle_Config")
+//      .show()
+//    personDF_split_cols.printSchema()
 //    personDF_split_cols.rdd
 //      .repartition(1)
 //      .map(_.toString())
@@ -108,7 +108,10 @@ object MLUseCase {
 //      .saveAsTextFile("target/cleandf/data")
 
 //    predictFatalityLR(cleanDF)
-    val NBTrainTestInput = personDFSubset.drop("LAT","LON")
+    val personDFSubset_dt = split_date_time(personDFSubset,"CRASH_DATETIME")
+    val personDFSubset_dt_drvr = split_columns_driver(personDFSubset_dt,"DRVR_CNTRB_CIRC_CL")
+    val NBTrainTestInput = personDFSubset_dt_drvr.drop("LAT","LON","CRASH_DATETIME","DRVR_CNTRB_CIRC_CL")
+
     NBTrainTestInput.printSchema()
     println("NB Input: ",NBTrainTestInput.show())
     ////////////////////////////////////////////////////////
@@ -123,20 +126,20 @@ object MLUseCase {
         , "VEHC_REG_STATE"
         , "WEATH_COND_DESCR"
         , "ROAD_SURF_COND_DESCR"
-        , "MANR_COLL_DESCR"
         , "FIRST_HRMF_EVENT_DESCR"
-        , "DRVR_CNTRB_CIRC_CL"
         , "VEHC_CONFG_DESCR"
-        , "HIT_RUN_DESCR"
         , "AGE_DRVR_YNGST"
         , "AGE_DRVR_OLDEST"
-        , "DRVR_DISTRACTED_CL"
         , "DRIVER_DISTRACTED_TYPE_DESCR"
         , "DRVR_LCN_STATE"
         , "DRUG_SUSPD_TYPE_DESCR"
-        , "SFTY_EQUP_DESC_1"
-        , "SFTY_EQUP_DESC_2"
-        , "ALC_SUSPD_TYPE_DESCR"))
+        , "ALC_SUSPD_TYPE_DESCR"
+        ,"First_DRVR_CNTRB_CIRC_CL"
+        ,"CRASH_MONTH"
+        ,"CRASH_DAY"
+        ,"CRASH_YEAR"
+        ,"CRASH_HOUR"
+      ))
       .setOutputCols(Array(
         "DISTRICT_NUM_index"
         ,"LCLTY_NAME_index"
@@ -145,22 +148,21 @@ object MLUseCase {
         , "VEHC_REG_STATE_index"
         , "WEATH_COND_DESCR_index"
         , "ROAD_SURF_COND_DESCR_index"
-        //        , "MAX_INJR_SVRTY_CL_index"
-        , "MANR_COLL_DESCR_index"
         , "FIRST_HRMF_EVENT_DESCR_index"
-        //        , "MOST_HRMFL_EVT_CL_index"
-        , "DRVR_CNTRB_CIRC_CL_index"
         , "VEHC_CONFG_DESCR_index"
-        , "HIT_RUN_DESCR_index"
         , "AGE_DRVR_YNGST_index"
         , "AGE_DRVR_OLDEST_index"
-        , "DRVR_DISTRACTED_CL_index"
         , "DRIVER_DISTRACTED_TYPE_DESCR_index"
         , "DRVR_LCN_STATE_index"
         , "DRUG_SUSPD_TYPE_DESCR_index"
-        , "SFTY_EQUP_DESC_1_index"
-        , "SFTY_EQUP_DESC_2_index"
-        , "ALC_SUSPD_TYPE_DESCR_index"))
+        , "ALC_SUSPD_TYPE_DESCR_index"
+        ,"First_DRVR_CNTRB_CIRC_CL_index"
+        ,"CRASH_MONTH_index"
+        ,"CRASH_DAY_index"
+        ,"CRASH_YEAR_index"
+        ,"CRASH_HOUR_index"
+      ))
+      .setHandleInvalid("keep")
 
     val indexed = indexer.fit(df1).transform(df1)
     indexed.show()
@@ -175,20 +177,19 @@ object MLUseCase {
         , "VEHC_REG_STATE_index"
         , "WEATH_COND_DESCR_index"
         , "ROAD_SURF_COND_DESCR_index"
-        , "MANR_COLL_DESCR_index"
         , "FIRST_HRMF_EVENT_DESCR_index"
-        , "DRVR_CNTRB_CIRC_CL_index"
         , "VEHC_CONFG_DESCR_index"
-        , "HIT_RUN_DESCR_index"
         , "AGE_DRVR_YNGST_index"
         , "AGE_DRVR_OLDEST_index"
-        , "DRVR_DISTRACTED_CL_index"
         , "DRIVER_DISTRACTED_TYPE_DESCR_index"
         , "DRVR_LCN_STATE_index"
         , "DRUG_SUSPD_TYPE_DESCR_index"
-        , "SFTY_EQUP_DESC_1_index"
-        , "SFTY_EQUP_DESC_2_index"
-        , "ALC_SUSPD_TYPE_DESCR_index"))
+        , "ALC_SUSPD_TYPE_DESCR_index"
+        ,"First_DRVR_CNTRB_CIRC_CL_index"
+        ,"CRASH_MONTH_index"
+        ,"CRASH_DAY_index"
+        ,"CRASH_YEAR_index"
+        ,"CRASH_HOUR_index"))
       .setOutputCol("features")
       .setHandleInvalid("skip")
     println("assembler: ", assembler)
@@ -211,27 +212,27 @@ object MLUseCase {
     println("predict_fatality_NB_model labels: ",predict_fatality_NB_model.labels)
     println("predict_fatality_NB_model modelType: ",predict_fatality_NB_model.modelType)
     val test_data = List((
-      6//"DISTRICT_NUM"
+      "6"//"DISTRICT_NUM"
       ,"DORCHESTER" //,"LCLTY_NAME"
       ,"NA"//,"OWNER_ADDR_CITY_TOWN"
       ,"NA"//, "OWNER_ADDR_STATE"
       ,"NA"//, "VEHC_REG_STATE"
       ,"Clear"//, "WEATH_COND_DESCR"
       ,"Dry"//, "ROAD_SURF_COND_DESCR"
-      ,"NA"//, "MANR_COLL_DESCR"
       ,"NA"//, "FIRST_HRMF_EVENT_DESCR"
-      ,"NA"//, "DRVR_CNTRB_CIRC_CL"
       ,"NA"//, "VEHC_CONFG_DESCR"
-      ,"NA"//, "HIT_RUN_DESCR"
       ,"NA"//, "AGE_DRVR_YNGST"
       ,"NA"//, "AGE_DRVR_OLDEST"
-      ,"NA"//, "DRVR_DISTRACTED_CL"
       ,"NA"//, "DRIVER_DISTRACTED_TYPE_DESCR"
       ,"NA"//, "DRVR_LCN_STATE"
       ,"NA"//, "DRUG_SUSPD_TYPE_DESCR"
-      ,"NA"//, "SFTY_EQUP_DESC_1"
-      ,"NA"//, "SFTY_EQUP_DESC_2"
       ,"NA"//, "ALC_SUSPD_TYPE_DESCR"
+      ,"Inattention"//,"First_DRVR_CNTRB_CIRC_CL"
+      ,"12"//,"CRASH_MONTH"
+      ,"10"//,"CRASH_DAY"
+      ,"20"//,"CRASH_YEAR"
+      ,"11"//,"CRASH_HOUR"
+
     ))
       .toDF(
       "DISTRICT_NUM"
@@ -241,20 +242,19 @@ object MLUseCase {
       , "VEHC_REG_STATE"
       , "WEATH_COND_DESCR"
       , "ROAD_SURF_COND_DESCR"
-      , "MANR_COLL_DESCR"
       , "FIRST_HRMF_EVENT_DESCR"
-      , "DRVR_CNTRB_CIRC_CL"
       , "VEHC_CONFG_DESCR"
-      , "HIT_RUN_DESCR"
       , "AGE_DRVR_YNGST"
       , "AGE_DRVR_OLDEST"
-      , "DRVR_DISTRACTED_CL"
       , "DRIVER_DISTRACTED_TYPE_DESCR"
       , "DRVR_LCN_STATE"
       , "DRUG_SUSPD_TYPE_DESCR"
-      , "SFTY_EQUP_DESC_1"
-      , "SFTY_EQUP_DESC_2"
       , "ALC_SUSPD_TYPE_DESCR"
+      , "First_DRVR_CNTRB_CIRC_CL"
+      , "CRASH_MONTH"
+      , "CRASH_DAY"
+      , "CRASH_YEAR"
+      , "CRASH_HOUR"
     )
     test_data.show()
 
@@ -281,156 +281,13 @@ object MLUseCase {
     val input_prediction = predict_fatality_NB_model.predict(assembled_test_data_vector)
     val input_prediction_prob = predict_fatality_NB_model.predictProbabilities(assembled_test_data_vector)
     println("predictions completed!")
-    input_prediction.take(10).foreach(x => println(x + " "))
+    input_prediction.toDF().show()
+//    input_prediction.take(10).foreach(x => println(x + " "))
 //    input_prediction.collect().foreach(println)
 //    println("input_prediction: ",input_prediction.collect().foreach(println))
 
 //    println("input_prediction_prob: ",input_prediction_prob.collect().foreach(println))
 
-  }
-
-  def filterAge(df:sql.DataFrame, age:Int) : sql.DataFrame = {
-    var df2 = df.filter(df("DRIVER_AGE") === age)
-    df2
-  }
-
-  def cleanData(df:sql.DataFrame) : sql.DataFrame = {
-    df.printSchema()
-    val df1 = df.na.fill("NA")
-    val indexer = new StringIndexer()
-      .setInputCols(Array(
-        "OWNER_ADDR_CITY_TOWN"
-        , "OWNER_ADDR_STATE"
-        , "VEHC_REG_STATE"
-        , "WEATH_COND_DESCR"
-        , "ROAD_SURF_COND_DESCR"
-//        , "MAX_INJR_SVRTY_CL"
-        , "MANR_COLL_DESCR"
-        , "FIRST_HRMF_EVENT_DESCR"
-//        , "MOST_HRMFL_EVT_CL"
-        , "DRVR_CNTRB_CIRC_CL"
-        , "VEHC_CONFG_DESCR"
-        , "HIT_RUN_DESCR"
-        , "AGE_DRVR_YNGST"
-        , "AGE_DRVR_OLDEST"
-        , "DRVR_DISTRACTED_CL"
-        , "DRIVER_DISTRACTED_TYPE_DESCR"
-        , "DRVR_LCN_STATE"
-        , "DRUG_SUSPD_TYPE_DESCR"
-        , "SFTY_EQUP_DESC_1"
-        , "SFTY_EQUP_DESC_2"
-        , "ALC_SUSPD_TYPE_DESCR"))
-      .setOutputCols(Array(
-        "OWNER_ADDR_CITY_TOWN_index"
-        , "OWNER_ADDR_STATE_index"
-        , "VEHC_REG_STATE_index"
-        , "WEATH_COND_DESCR_index"
-        , "ROAD_SURF_COND_DESCR_index"
-//        , "MAX_INJR_SVRTY_CL_index"
-        , "MANR_COLL_DESCR_index"
-        , "FIRST_HRMF_EVENT_DESCR_index"
-//        , "MOST_HRMFL_EVT_CL_index"
-        , "DRVR_CNTRB_CIRC_CL_index"
-        , "VEHC_CONFG_DESCR_index"
-        , "HIT_RUN_DESCR_index"
-        , "AGE_DRVR_YNGST_index"
-        , "AGE_DRVR_OLDEST_index"
-        , "DRVR_DISTRACTED_CL_index"
-        , "DRIVER_DISTRACTED_TYPE_DESCR_index"
-        , "DRVR_LCN_STATE_index"
-        , "DRUG_SUSPD_TYPE_DESCR_index"
-        , "SFTY_EQUP_DESC_1_index"
-        , "SFTY_EQUP_DESC_2_index"
-        , "ALC_SUSPD_TYPE_DESCR_index"))
-
-    val indexed = indexer.fit(df1).transform(df1)
-    indexed.show()
-
-
-    val assembler = new VectorAssembler()
-      .setInputCols(Array("LAT"
-        , "LON"
-        , "OWNER_ADDR_CITY_TOWN_index"
-        , "OWNER_ADDR_STATE_index"
-        , "VEHC_REG_STATE_index"
-        , "WEATH_COND_DESCR_index"
-        , "ROAD_SURF_COND_DESCR_index"
-//        , "MAX_INJR_SVRTY_CL_index"
-        , "MANR_COLL_DESCR_index"
-        , "FIRST_HRMF_EVENT_DESCR_index"
-//        , "MOST_HRMFL_EVT_CL_index"
-        , "DRVR_CNTRB_CIRC_CL_index"
-        , "VEHC_CONFG_DESCR_index"
-        , "HIT_RUN_DESCR_index"
-        , "AGE_DRVR_YNGST_index"
-        , "AGE_DRVR_OLDEST_index"
-        , "DRVR_DISTRACTED_CL_index"
-        , "DRIVER_DISTRACTED_TYPE_DESCR_index"
-        , "DRVR_LCN_STATE_index"
-        , "DRUG_SUSPD_TYPE_DESCR_index"
-        , "SFTY_EQUP_DESC_1_index"
-        , "SFTY_EQUP_DESC_2_index"
-        , "ALC_SUSPD_TYPE_DESCR_index"))
-      .setOutputCol("features")
-      .setHandleInvalid("skip")
-    println("assembler: ", assembler)
-    val df3 = assembler.transform(indexed).select(col("FATALITY_BIN").cast(DoubleType).as("label"), col("features"))
-    df3
-  }
-
-  def predictFatalityLR(df:sql.DataFrame) : Unit = {
-
-    println("assembler count from df3: ",df.count())
-//    df3.write.mode('append').json("target\assembler.json")
-    print("creating Logistic Regression Model")
-    val lr = new LogisticRegression()
-      .setMaxIter(10)
-      .setRegParam(0.3)
-      .setElasticNetParam(0.8)
-    println("set params")
-    // Fit the model
-    val lrModel = lr.fit(df)
-    println("fit")
-    // Print the coefficients and intercept for logistic regression
-    println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
-
-    // We can also use the multinomial family for binary classification
-    val mlr = new LogisticRegression()
-      .setMaxIter(10)
-      .setRegParam(0.3)
-      .setElasticNetParam(0.8)
-      .setFamily("multinomial")
-
-    val mlrModel = mlr.fit(df)
-
-    // Print the coefficients and intercepts for logistic regression with multinomial family
-    println(s"Multinomial coefficients: ${mlrModel.coefficientMatrix}")
-    println(s"Multinomial intercepts: ${mlrModel.interceptVector}")
-
-
-    /*
-    Summary
-     */
-    // Extract the summary from the returned LogisticRegressionModel instance trained in the earlier
-    // example
-    val trainingSummary = lrModel.binarySummary
-
-    // Obtain the objective per iteration.
-    val objectiveHistory = trainingSummary.objectiveHistory
-    println("objectiveHistory:")
-    objectiveHistory.foreach(loss => println(loss))
-
-    // Obtain the receiver-operating characteristic as a dataframe and areaUnderROC.
-    val roc = trainingSummary.roc
-    roc.show()
-    println(s"areaUnderROC: ${trainingSummary.areaUnderROC}")
-
-//    // Set the model threshold to maximize F-Measure
-//    val fMeasure = trainingSummary.fMeasureByThreshold
-//    val maxFMeasure = fMeasure.select(max("F-Measure")).head().getDouble(0)
-//    val bestThreshold = fMeasure.where(col("F-Measure") === maxFMeasure)
-//      .select("threshold").head().getDouble(0)
-//    lrModel.setThreshold(bestThreshold)
   }
 
 
@@ -466,6 +323,27 @@ object MLUseCase {
     var dataframe = df.na.fill("V1:()", Array(column_name))
     dataframe = dataframe.withColumn("Vehicle_One", split(col(column_name),"\\)").getItem(0))
     dataframe = dataframe.withColumn("Vehicle_Config",regexp_replace(col("Vehicle_One"),"V1:\\(",""))
+    dataframe.show()
+    dataframe
+  }
+  def split_date_time(df: sql.DataFrame, column_name: String) :sql.DataFrame = {
+    var dataframe = df.withColumn("CRASH_MONTH", split(col(column_name),"[\\/]").getItem(0))
+      .withColumn("CRASH_DAY", split(col(column_name),"[\\/]").getItem(1))
+      .withColumn("CRASH_YEAR_TIME", split(col(column_name),"[\\/]").getItem(2))
+    dataframe = dataframe.withColumn("CRASH_YEAR", split(col("CRASH_YEAR_TIME"),"\\ ").getItem(0))
+    dataframe = dataframe.withColumn("TIME", split(col("CRASH_YEAR_TIME"),"\\ ").getItem(1)).drop("CRASH_YEAR_TIME")
+    dataframe = dataframe.withColumn("CRASH_HOUR", split(col("TIME"),"\\:").getItem(0)).drop("TIME")
+    dataframe.show()
+    dataframe
+  }
+
+  def split_columns_driver(df: sql.DataFrame, column_name: String) :sql.DataFrame = {
+    var dataframe = df.na.fill("D1:()", Array(column_name))
+    dataframe = dataframe.withColumn("Driver_One", split(col(column_name),"[\\)]*[\\/]").getItem(0))
+    dataframe = dataframe.withColumn("Driver_Control",regexp_replace(col("Driver_One"),"[D1:]*[D2:]*[D3:]*[D4:]","")).drop("Driver_One")
+    dataframe = dataframe.withColumn("First",regexp_replace(col("Driver_Control"),"[\\(]*[\\)]","")).drop("Driver_Control")
+    dataframe = dataframe.withColumn("First_" + column_name,regexp_replace(col("First"),"[\\(]","")).drop("First")
+    //dataframe.coalesce(1).write.option("header", "true").csv("src/splitDriver.csv")
     dataframe.show()
     dataframe
   }
