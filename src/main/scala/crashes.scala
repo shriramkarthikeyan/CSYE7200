70936521e4f403ce99a0f359e8b1928f44a3b4f7
akaushik12 <47017850+akaushik12@users.noreply.github.com>
2020-12-11 21:26:03 -0500

    Commented Print Statements

--- src/main/scala/crashes.scala
+++ src/main/scala/crashes.scala
@@ -40,7 +40,7 @@ class crashes(locality: String){
       (if (road != null) desiredThings = desiredThings + ("ROAD_SURF_COND_DESCR" -> road))
 
       var df_c = df.filter(col("DRIVER_AGE").like("-99"))
-      println("desiredThings = " + desiredThings)
+      //println("desiredThings = " + desiredThings)
 
       breakable
       {
@@ -52,36 +52,13 @@ class crashes(locality: String){
 
         }
       }
-//      desiredThings.keys.foreach { i =>
-//        df_c = df_c.filter(col(i).isNotNull)
-//      }
-//
-//      println("union :" + df_c.count())
-//
-////      println("crash df")
-////      val crash_df2 = df.filter(
-////        col("LCLTY_NAME").like(locality) &&
-////        (col("DRIVER_AGE").like(age.toString)) //&&
-////        //(col("VEHC_CONFIG_CL").like(vehicle))
-////      )
-////      crash_df2.count()
-////      crash_df2.show()
-//
-//      df_c.createOrReplaceTempView("crashes")
-//      val crash_df = spark.sql(
-//        "select * " +
-//          "from crashes "+
-//          "group by STREET_NUMB,RDWY,DIST_DIRC_FROM_INT,NEAR_INT_RDWY,DIST_DIRC_EXIT,LAT,LON,DISTRICT_NUM,LCLTY_NAME,OWNER_ADDR_CITY_TOWN,OWNER_ADDR_STATE,VEHC_REG_STATE,WEATH_COND_DESCR,ROAD_SURF_COND_DESCR,MAX_INJR_SVRTY_CL,MANR_COLL_DESCR,FIRST_HRMF_EVENT_DESCR,MOST_HRMFL_EVT_CL,DRVR_CNTRB_CIRC_CL,VEHC_CONFIG_CL,HIT_RUN_DESCR,AGE_DRVR_YNGST,AGE_DRVR_OLDEST,DRVR_DISTRACTED_CL,DRIVER_AGE,DRIVER_DISTRACTED_TYPE_DESCR,DRVR_LCN_STATE,DRUG_SUSPD_TYPE_DESCR,SFTY_EQUP_DESC_1,SFTY_EQUP_DESC_2,ALC_SUSPD_TYPE_DESCR " +
-//          "having count(*) > 1")
-//
-//      crash_df.show()
-//      crash_df
-      data.count()
+
+      //println("Number of records = " + data.count())
       data.show()
       data
     }
     catch {
-      case e: Exception => println("No records found", e)
+      case e: Exception => //println("No records found", e)
         val dataf = spark.emptyDataFrame
         dataf
     }
